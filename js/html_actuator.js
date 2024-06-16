bbb9ad22d9d37240ac51409e24a5ebc9760daab0
Ecaly <113827820+Ecaly@users.noreply.github.com>
2024-06-16 17:28:55 +0100

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -68,6 +68,10 @@ HTMLActuator.prototype.addTile = function (tile) {
 
   inner.classList.add("tile-inner");
   inner.textContent = tile.value;
+  if (tile.value === 18) inner.textContent = "ツ";
+  if (tile.value === 19) inner.textContent = "㋡";
+  if (tile.value === 20) inner.textContent = "!!";
+  if (tile.value === 21) inner.textContent = "⦾";
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
